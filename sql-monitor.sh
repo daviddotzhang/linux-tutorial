27307c20415604c4f23fff220e454bc9c17d83c4
daviddotzhang <daviddotzhang@liferay.com>
2017-03-01 13:16:32 +0700

    linux - this script used monitor mysql network traffic.echo sql

---
+++ sql-monitor.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#this script used monitor mysql network traffic.echo sql
+tcpdump -i any -s 0 -l -w - dst port 3306 | strings | perl -e '
+while(<>) { chomp; next if /^[^ ]+[ ]*$/;
+    if(/^(SELECT|INSERT|UPDATE|DELETE)/i)
+    {
+        if (defined $q) { print "$q\n"; }
+        $q=$_;
+    } else {
+        $_ =~ s/^[ \t]+//; $q.=" $_";
+    }
+}'
